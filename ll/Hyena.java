b99a70d17f8a5fe24d8038884103c31bf87bd5c7
mathieu-gillin <60882043+mathieu-gillin@users.noreply.github.com>
2020-02-10 12:45:31 +0000

    Add files via upload

---
+++ ll/Hyena.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Hyena here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Hyena extends Predator
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Hyena
+     */
+    public Hyena()
+    {
+        
+    }
+
+}
